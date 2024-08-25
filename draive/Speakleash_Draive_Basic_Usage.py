73485ce3d622da12aed2df374c3aa3cb361736a3
IgorTest19 <58894110+IgorTest19@users.noreply.github.com>
2024-08-25 20:53:20 +0200

    cleaning up file (#18)

--- draive/Speakleash_Draive_Basic_Usage.py
+++ draive/Speakleash_Draive_Basic_Usage.py
@@ -1,3 +1,7 @@
+"""
+Poniższy kod jest adaptacją przykładu z biblioteki Draive znajdującego się pod linkiem:
+https://github.com/miquido/draive/blob/main/guides/BasicUsage.ipynb
+"""
 from asyncio import run
 from draive import LMM, ctx, generate_text
 from draive import setup_logging
@@ -5,34 +9,35 @@ from draive.ollama import OllamaClient, ollama_lmm_invocation, OllamaChatConfig
 
 setup_logging("text_completion")
 
-async def main(provided_model, provided_temp) -> None:
 
+async def main(provided_model, provided_temp) -> None:
     async def text_completion(text: str) -> str:
-        # generate_text is a simple interface for generating text
+        # generate_text to prosty interfejs przeznaczony do generowania tekstu
         return await generate_text(
-            # We have to provide instructions / system prompt to instruct the model
-            instruction="Prepare the simplest completion of a given text",
-            # input is provided separately
-            input=text,
+                # Należy podać instrukcje / systemowy prompt, aby poinstruować model
+                instruction="Prepare the simplest completion of a given text",
+                # wejście jest podawane oddzielnie od instrukcji
+                input=text,
         )
 
-    async with ctx.new(  # prepare new context
-        "text_completion",
-        state=[
-                LMM(invocation=ollama_lmm_invocation),
-                OllamaChatConfig(
-                        model=provided_model,
-                        temperature=provided_temp,
-                )
-               ],  # set currently used LMM to OpenAI
-        dependencies=[OllamaClient],  # use OpenAIClient dependency for accessing OpenAI services
+    async with ctx.new(  # przygotowanie nowego kontekstu
+            "text_completion",
+            state=[
+                    LMM(invocation=ollama_lmm_invocation),
+                    OllamaChatConfig(
+                            model=provided_model,
+                            temperature=provided_temp,
+                    )
+            ],
+            dependencies=[OllamaClient],
     ):
         result: str = await text_completion(
-            text="Roses are red...",
+                text="Z jakich składników robi się sękacza?.",
         )
 
         print(f'RESULT {provided_model} | temperature {provided_temp}:\n{result}')
 
-model = 'SpeakLeash/bielik-7b-instruct-v0.1-gguf'
-temperature = 0.2
-run(main=main(model, temperature))
+
+MODEL = 'SpeakLeash/bielik-7b-instruct-v0.1-gguf'
+TEMPERATURE = 0.2
+run(main=main(MODEL, TEMPERATURE))
