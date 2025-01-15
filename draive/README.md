76b7fb54734f03deaa7ddfcf6e980304ac5b6255
Kacper Kaliński <47140412+KaQuMiQ@users.noreply.github.com>
2025-01-15 14:23:48 +0100

    Update draive example (#59)

--- draive/README.md
+++ draive/README.md
@@ -1,3 +1,7 @@
 # Bielik + Draive
 Zaprezentowane przykłady stanowią integrację różnych wersji modelu Bielik z biblioteką draive, którą można znaleźć pod nastepującym adresem:<br>
 https://github.com/miquido/draive/tree/main
+
+## Uruchomienie z uv
+
+Plik można uruchomić bezpośrednio przy pomocy narzędzia [uv](https://github.com/astral-sh/uv) używając komendy `uv run` (`uv run Speakleash_Draive_Basic_Usage.py`) która w locie przygotuje środowisko wraz zależnościami. Aby pobrać uv można użyć komendy `curl -LsSf https://astral.sh/uv/install.sh | sh` lub (dla windows) `powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"`.
--- draive/Speakleash_Draive_Basic_Usage.py
+++ draive/Speakleash_Draive_Basic_Usage.py
@@ -1,11 +1,19 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "draive[ollama]~=0.37",
+# ]
+# ///
+#
 """
 Poniższy kod jest adaptacją przykładu z biblioteki Draive znajdującego się pod linkiem:
 https://github.com/miquido/draive/blob/main/guides/BasicUsage.ipynb
 """
+
 from asyncio import run
-from draive import LMM, ctx, generate_text
-from draive import setup_logging
-from draive.ollama import OllamaClient, ollama_lmm_invocation, OllamaChatConfig
+
+from draive import ctx, generate_text, setup_logging
+from draive.ollama import OllamaChatConfig, ollama_lmm
 
 setup_logging("text_completion")
 
@@ -14,31 +22,29 @@ async def main(provided_model, provided_temp) -> None:
     async def text_completion(text: str) -> str:
         # generate_text to prosty interfejs przeznaczony do generowania tekstu
         return await generate_text(
-                # Należy podać instrukcje / systemowy prompt, aby poinstruować model
-                instruction="Prepare the simplest completion of a given text",
-                # wejście jest podawane oddzielnie od instrukcji
-                input=text,
+            # Należy podać instrukcje / systemowy prompt, aby poinstruować model
+            instruction="Prepare the simplest completion of a given text",
+            # wejście jest podawane oddzielnie od instrukcji
+            input=text,
         )
 
-    async with ctx.new(  # przygotowanie nowego kontekstu
-            "text_completion",
-            state=[
-                    LMM(invocation=ollama_lmm_invocation),
-                    OllamaChatConfig(
-                            model=provided_model,
-                            temperature=provided_temp,
-                    )
-            ],
-            dependencies=[OllamaClient],
+    async with ctx.scope(  # przygotowanie nowego kontekstu
+        "text_completion",
+        ollama_lmm(),  # użycie ollama jako llm w kontekście
+        OllamaChatConfig(
+            model=provided_model,
+            temperature=provided_temp,
+        ),
     ):
         result: str = await text_completion(
-                text="Z jakich składników robi się sękacza?.",
+            text="Z jakich składników robi się sękacza?.",
         )
 
-        print(f'RESULT {provided_model} | temperature {provided_temp}:\n{result}')
+        print(f"RESULT {provided_model} | temperature {provided_temp}:\n{result}")
+
 
 # Wybież jeden z poniższych modeli do generowania odpowiedzi
-MODEL_V1 = 'SpeakLeash/bielik-7b-instruct-v0.1-gguf:Q4_K_S'
-MODEL_V2 = 'SpeakLeash/bielik-11b-v2.2-instruct:Q4_K_M'
+MODEL_V1 = "SpeakLeash/bielik-7b-instruct-v0.1-gguf:Q4_K_S"
+MODEL_V2 = "SpeakLeash/bielik-11b-v2.2-instruct:Q4_K_M"
 TEMPERATURE = 0.2
 run(main=main(MODEL_V2, TEMPERATURE))
